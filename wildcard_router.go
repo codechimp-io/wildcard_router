0c5850f7e38aa299f3e760d532830cf336de86f0
Boris V <bgv@users.noreply.github.com>
2019-07-27 12:47:44 +0100

    Update wildcard_router.go

--- wildcard_router.go
+++ wildcard_router.go
@@ -96,7 +96,7 @@ func (w *WildcardRouterWriter) Write(data []byte) (int, error) {
 
 func (w *WildcardRouterWriter) reset() {
 	w.skipNotFoundCheck = false
-	w.Header().Set("Content-Type", "")
+//	w.Header().Set("Content-Type", "")
 	w.status = 0
 }
 
